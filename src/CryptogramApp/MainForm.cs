15a3b7e3d1e88af7be12d9aaf89186f43244b60a
Charles Farris <charlesfarris71@gmail.com>
2022-12-04 10:28:35 -0700

    Create Windows Form application
    - Add projects
    - Fix R# issues

---
+++ src/CryptogramApp/MainForm.cs
@@ -0,0 +1,15 @@
+namespace SleepingBearSystems.Cryptogram.CryptogramApp;
+
+/// <summary>
+/// Application Main Form.
+/// </summary>
+public partial class MainForm : Form
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public MainForm()
+    {
+        this.InitializeComponent();
+    }
+}
